d3c67cc1ec1b25821e77931a1b669f9cdbd4ab4a
CodeXrook <lacekitfly@gmail.com>
2020-10-04 19:32:50 -0400

    wooooow stop timer done sonnnnn haha yooooooo, i gotta style this mf doee

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -2,68 +2,64 @@ import React, { Component } from "react";
 import "./App.css";
 
 class Stopwatch extends Component {
-    state = {
-        timerOn: false,
-        timerStart: 0,
-        timerTimer: 0
-    };
+  state = {
+    timerOn: false,
+    timerStart: 0,
+    timerTime: 0
+  };
 
-    startTimer = () => {
-        this.setState({
-            timerOn: true,
-            timerTime: this.state.timerTime,
-            timerStart: Date.now() - this.state.timerTimer
-        });
-        this.timer = setInterval(() => {
-            this.setState({
-                timerTime: Date.now() - this.state.timerStart
-            });
-        }, 10);
-    };
+  startTimer = () => {
+    this.setState({
+      timerOn: true,
+      timerTime: this.state.timerTime,
+      timerStart: Date.now() - this.state.timerTime
+    });
+    this.timer = setInterval(() => {
+      this.setState({
+        timerTime: Date.now() - this.state.timerStart
+      });
+    }, 10);
+  };
 
-    stopTimer = () => {
-        this.setState({ timerOn: false });
-        clearInterval(this.timer);
-    };
+  stopTimer = () => {
+    this.setState({ timerOn: false });
+    clearInterval(this.timer);
+  };
 
-    resetTimer = () => {
-        this.setState({
-            timerStart: 0,
-            timerTime: 0
-        });
-    };
+  resetTimer = () => {
+    this.setState({
+      timerStart: 0,
+      timerTime: 0
+    });
+  };
 
-    render() {
-        const { timerTime } = this.state;
-        let centiseconds = ("0" + (Math.floor(timerTime / 10 ) % 
-        100)).slice(-2);
-        let seconds = ("0" + (Math.floor(timerTime / 1000) % 60)).slice(-2);
-        let minutes = ("0" + (Math.floor(timerTime / 60000) %
-        60)).slice(-2);
-        let hours = ("0" + Math.floor(timerTime / 3600000)).slice(-2);
-
-        return (
-            <div className="Stopwatch">
-              <div className="Stopwatch-header">Stopwatch</div>
-                 <div className="Stopwatch-display">
-                    {hours} : {minutes} : {seconds} : {centiseconds}
-            </div>
-            {this.state.timerOn === false && this.state.timerTime === 0 && (
-              <button onClick={this.startTimer}>Start</button>
-            )}
-            {this.state.timerOn === true && (
-              <button onClick={this.stopTimer}>Stop</button>
-            )}
-            {this.state.timerOn === false && this.state.timerTime > 0 && (
-                <button onClick={this.startTimer}>Resume</button>
-            )}
-            {this.state.timerOn === false && this.state.timerTime > 0 && (
-                <button onClick={this.resetTimer}>Reset</button>
-            )}
-
-          </div>
-        );
-    }
+  render() {
+    const { timerTime } = this.state;
+    let centiseconds = ("0" + (Math.floor(timerTime / 10) % 100)).slice(-2);
+    let seconds = ("0" + (Math.floor(timerTime / 1000) % 60)).slice(-2);
+    let minutes = ("0" + (Math.floor(timerTime / 60000) % 60)).slice(-2);
+    let hours = ("0" + Math.floor(timerTime / 3600000)).slice(-2);
+    return (
+      <div className="Stopwatch">
+        <div className="Stopwatch-header">Stopwatch</div>
+        <div className="Stopwatch-display">
+          {hours} : {minutes} : {seconds} : {centiseconds}
+        </div>
+        {this.state.timerOn === false && this.state.timerTime === 0 && (
+          <button onClick={this.startTimer}>Start</button>
+        )}
+        {this.state.timerOn === true && (
+          <button onClick={this.stopTimer}>Stop</button>
+        )}
+        {this.state.timerOn === false && this.state.timerTime > 0 && (
+          <button onClick={this.startTimer}>Resume</button>
+        )}
+        {this.state.timerOn === false && this.state.timerTime > 0 && (
+          <button onClick={this.resetTimer}>Reset</button>
+        )}
+      </div>
+    );
+  }
 }
 
 export default Stopwatch;
