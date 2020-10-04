2520a86bce65ef50a44c8a9943e84851a44f972f
CodeXrook <lacekitfly@gmail.com>
2020-10-04 19:28:45 -0400

    set timer

--- src/Countdown.js
+++ src/Countdown.js
@@ -15,7 +15,7 @@ class Countdown extends Component {
             timerStart: this.state.timerTime
         });
         this.timer = setInterval(() => {
-            const newTime = this.state.timerTimer -10;
+            const newTime = this.state.timerTimer - 10;
             if (newTime >= 0) {
                 this.setState({
                     timerTime: newTime
@@ -33,7 +33,7 @@ class Countdown extends Component {
         this.setState({ timerOn: false });
     };
     resetTimer = () => {
-        if (this.state.timerOn === false ) {
+        if (this.state.timerOn === false) {
             this.setState({
                 timerTime: this.state.timerStart
             });
@@ -45,7 +45,7 @@ class Countdown extends Component {
         const max = 216000000;
         if (!timerOn) {
             if (input === "incHours" && timerTime + 3600000 < max) {
-                this.setState({ timerTime: timerTime + 3600000});
+                this.setState({ timerTime: timerTime + 3600000 });
             } else if (input === "decHours" && timerTime - 3600000 >= 0) {
                 this.setState({ timerTime: timerTime - 3600000 });
             } else if (input === "incMinutes" && timerTime + 60000 < max) {
@@ -54,25 +54,42 @@ class Countdown extends Component {
                 this.setState({ timerTime: timerTime - 60000 });
             } else if (input === "incSeconds" && timerTime + 1000 < max) {
                 this.setState({ timerTime: timerTime + 1000 });
-            } else if (input === "decSeconds" && timerTime - 1000 >= 0 ) {
+            } else if (input === "decSeconds" && timerTime - 1000 >= 0) {
                 this.setState({ timerTime: timerTime - 1000 });
             }
         }
     };
 
     render() {
-        const { timerTime, timerStart, timerOn } =  this.state;
-        let seconds = ("0" + (Math.floor((timerTime / 1000 % 60) %
-        60)).slice(-2);
-        let minutes = ("0" + Math.floor((timerTime / 60000 ) % 
-        60)).slice(-2);
-        let hours = ("0" + Math.floor((timerTIme / 3600000) % 
-        60)).slice(-2);
+        const { timerTime, timerStart, timerOn } = this.state;
+        let seconds = ("0" + (Math.floor((timerTime / 1000) % 60) %
+            60)).slice(-2);
+        let minutes = ("0" + Math.floor((timerTime / 60000) %
+            60)).slice(-2);
+        let hours = ("0" + Math.floor((timerTime / 3600000) %
+            60)).slice(-2);
 
         return (
             <div className="Countdown">
                 <div className="Countdown-header">Countdown</div>
+                <div className="Countdown-label">Hours : Minutes : Seconds</div>
+                <div className="Countdown-display">
+                    <button onClick={() => this.adjustTimer("incHours")}>&#8679;</button>
+                    <button onClick={() => this.adjustTimer("incMinutes")}>&#8679;</button>
+                    <button onClick={() => this.adjustTimer("incSeconds")}>&#8679;</button>
+
+                    <div className="Countdown-time">
+                        {hours} : {minutes} : {seconds}
+                    </div>
+
+                    <button onClick={() => this.adjustTimer("decHours")}>&#8681;</button>
+                    <button onClick={() => this.adjustTimer("decMinutes")}>&#8681;</button>
+                    <button onClick={() => this.adjustTimer("decSeconds")}>&#8681;</button>
+                </div>
+
+                
             </div>
+
         );
     }
 }
