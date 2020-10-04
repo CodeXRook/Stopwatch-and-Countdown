86900faddb22a5bbb66862858d5e04476abc8e31
CodeXrook <lacekitfly@gmail.com>
2020-10-04 17:45:14 -0400

    created and setup stopwatch component/file, need timers file

---
+++ src/Stopwatch.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class Stopwatch extends Component {
+    render() {
+        return (
+            <div className="Stopwatch">
+                <div className="Stopwatch-header">Stopwatch</div>
+            </div>
+        );
+    }
+}
+
+export default Stopwatch;
