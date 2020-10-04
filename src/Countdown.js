d8f7af6257710967731f1f0c70b504c1eb0dd45b
CodeXrook <lacekitfly@gmail.com>
2020-10-04 18:43:54 -0400

    Starting the Countdown timer

--- src/Countdown.js
+++ src/Countdown.js
@@ -8,7 +8,25 @@ class Countdown extends Component {
         timerTimer: 0
     };
 
-    startTimer 
+    startTimer = () => {
+        this.setState({
+            timerOn: true,
+            timerTime: this.state.timerTime,
+            timerStart: this.state.timerTime
+        });
+        this.timer = setInterval(() => {
+            const newTime = this.state.timerTimer -10;
+            if (newTime >= 0) {
+                this.setState({
+                    timerTime: newTime
+                });
+            } else {
+                clearInterval(this.timer);
+                this.setState({ timerOn: false });
+                alert("Countdown ended");
+            }
+        }, 10);
+    };
 
     render() {
         return (
