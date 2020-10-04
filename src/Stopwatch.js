c572d23e6ea48795a13f9522087ec0a65ce62bd6
CodeXrook <lacekitfly@gmail.com>
2020-10-04 18:23:36 -0400

    need to display timer

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -21,11 +21,35 @@ class Stopwatch extends Component {
         }, 10);
     };
 
+    stopTimer = () => {
+        this.setState({ timerOn: false });
+        clearInterval(this.timer);
+    };
+
+    resetTimer = () => {
+        this.setState({
+            timerStart: 0,
+            timerTime: 0
+        });
+    };
+
     render() {
+        const { timerTime } = this.state;
+        let centiseconds = ("0" + (Math.floor(timerTime / 10 ) % 
+        100)).slice(-2);
+        let seconds = ("0" + (Math.floor(timerTime / 1000) % 60)).slice(-2);
+        let minutes = ("0" + (Math.floor(timerTime / 60000) %
+        60)).slice(-2);
+        let hours = ("0" + Math.floor(timerTime / 3600000)).slice(-2);
+
         return (
             <div className="Stopwatch">
-                <div className="Stopwatch-header">Stopwatch</div>
+              <div className="Stopwatch-header">Stopwatch</div>
+                 <div className="Stopwatch-display">
+                    {hours} : {minutes} : {seconds} : {centiseconds}
             </div>
+            
+          </div>
         );
     }
 }
