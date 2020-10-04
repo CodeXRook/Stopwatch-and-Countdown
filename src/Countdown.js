e9260daad0afca5732a6d54ad99c397ec498f45a
CodeXrook <lacekitfly@gmail.com>
2020-10-04 17:48:32 -0400

    Countdown added, loking nice, lets work.

---
+++ src/Countdown.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class Countdown extends Component {
+    render() {
+        return (
+            <div className="Countdown">
+                <div className="Countdown-header">Countdown</div>
+            </div>
+        );
+    }
+}
+
+export default Countdown;
